0fb4a357ebe611a2562c886b74d53f2e3c82f3f8
nrthbnd <asoloshenkova@gmail.com>
2023-11-23 23:08:38 +0300

    Add check_wish_not_completed_or_reserved

--- app/api/validators.py
+++ app/api/validators.py
@@ -1,11 +1,13 @@
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from constants import (
     NAME_DUPLICATE_EXCEPTION, WISH_NOT_EXISTS_EXCEPTION,
+    WISH_ALREADY_RESERVED, WISH_ALREADY_COMPLETED,
 )
 from app.crud.wish import wish_crud
-from app.models import Wish
+from app.models import Wish, Reservation
 
 
 async def check_name_duplicate(
@@ -37,3 +39,45 @@ async def check_wish_exists(
             detail=WISH_NOT_EXISTS_EXCEPTION,
         )
     return wish
+
+
+async def check_wish_not_completed_or_reserved(
+        wish_id: int,
+        session: AsyncSession,
+):
+    """Проверить, не было ли желание выполнено или забронировано."""
+    # Проверка в таблице бронирований
+    was_reserved = await session.execute(
+        select(Reservation).where(
+             Reservation.wish_id == wish_id,
+        )
+    )
+
+    if was_reserved.scalars().first() is not None:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=WISH_ALREADY_RESERVED,
+        )
+
+    # Проверка в таблице пожеланий
+    fields = await session.execute(
+        select(
+            Wish.reserved,
+            Wish.completed,
+        ).where(
+            Wish.id == wish_id,
+        )
+    )
+    fields = fields.all()
+    fields = dict(fields[0])
+
+    if fields['reserved'] is not False:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=WISH_ALREADY_RESERVED,
+        )
+    if fields['completed'] is not False:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=WISH_ALREADY_COMPLETED,
+        )
