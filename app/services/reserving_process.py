93ac58c1daa86a7361f8818ce58b4f6f8309e48f
nrthbnd <asoloshenkova@gmail.com>
2023-11-27 18:24:13 +0300

    Add flag new_reservation

--- app/services/reserving_process.py
+++ app/services/reserving_process.py
@@ -1,16 +1,33 @@
+from datetime import datetime
+
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models import Wish
+from app.crud.reservation import reservation_crud
+from constants import SWITCH_FIELD_COMPLETED
 
 
 async def change_wish_status(
     wish: Wish,
     field_to_switch: str,
+    new_reservation: bool,
     session: AsyncSession,
 ):
     """Изменить статус пожелания."""
     current_value = getattr(wish, field_to_switch)
     new_value = not current_value
     setattr(wish, field_to_switch, new_value)
+
+    if new_reservation is False:
+        reservation = await reservation_crud.get_reservation_by_wish_id(
+            wish.id, session,
+        )
+        if field_to_switch == SWITCH_FIELD_COMPLETED and new_value is True:
+            wish.close_date = datetime.now()
+            if reservation:
+                reservation.close_date = datetime.now()
+        else:
+            wish.close_date, reservation.close_date = None, None
 
     session.add(wish)
     await session.commit()
