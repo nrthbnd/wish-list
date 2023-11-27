406beac8052bb83f0e9f0d6c0f7637165f16f5e6
nrthbnd <asoloshenkova@gmail.com>
2023-11-27 18:27:20 +0300

    Updates

--- app/crud/reservation.py
+++ app/crud/reservation.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import CRUDBase
 from app.models.reservation import Reservation
@@ -5,6 +8,19 @@ from app.models.reservation import Reservation
 
 class CRUDReservation(CRUDBase):
     """CRUD-операции с бронированиями."""
+
+    async def get_reservation_by_wish_id(
+            self,
+            wish_id: int,
+            session: AsyncSession,
+    ) -> Optional[Reservation]:
+        """Получить бронирование по id пожелания."""
+        db_reservation = await session.execute(
+            select(Reservation).where(
+                Reservation.wish_id == wish_id
+            )
+        )
+        return db_reservation.scalars().first()
 
 
 reservation_crud = CRUDReservation(Reservation)
