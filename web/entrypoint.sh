2f40fef3c60fbb8a69b6037e9b598af74978b483
Michael Goldberg <deutschman1999@mail.ru>
2024-04-03 17:03:26 +0300

    test entrypoint not healthy

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,19 +1,19 @@
-##!/bin/sh
-#
-#if [ "$DATABASE" = "wish-list_db" ]
-#then
-#    echo "Waiting for postgres..."
-#
-#    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
-#      sleep 0.1
-#    done
-#
-#    echo "PostgreSQL started"
-#fi
-## shellcheck disable=SC1083
-##uvicorn project.web.main:web --reload --host=0.0.0.0 --port="$PORT 8000"
-#alembic revision --autogenerate -m "Initianal migration"
-#alembic upgrade head
-#uvicorn main:app --reload --host=0.0.0.0 --port=8000
-#
-#exec "$@"
+#!/bin/sh
+
+if [ "$DATABASE" = "wish-list_db" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+# shellcheck disable=SC1083
+#uvicorn project.web.main:web --reload --host=0.0.0.0 --port="$PORT 8000"
+alembic revision --autogenerate -m "Initianal migration"
+alembic upgrade head
+uvicorn main:app --reload --host=0.0.0.0 --port=8000
+
+exec "$@"
